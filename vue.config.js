33948fe930e60537e1c13e673b7ce78c9a0957bb
Nelio Carneiro <neliojrr@gmail.com>
2018-11-26 10:22:48 +0100

    add prerender

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,24 @@
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: ['/', '/contato', '/login', '/obrigado'],
+      }),
+    ],
+  },
   lintOnSave: false,
   chainWebpack: (config) => {
     config.module
       .rule('i18n')
       .resourceQuery(/blockType=i18n/)
       .use('i18n')
-        .loader('@kazupon/vue-i18n-loader')
-        .end();
-  }
+      .loader('@kazupon/vue-i18n-loader')
+      .end();
+  },
 };
