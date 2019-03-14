f012a7f31d95565b74095af1f7220cc85607898a
Nelio Carneiro <neliojrr@gmail.com>
2019-03-14 20:31:36 -0300

    Add search for clients

--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -6,7 +6,12 @@
       <div class="top-actions columns is-mobile">
         <div class="field client-search column is-half-desktop">
           <div class="control has-icons-left">
-            <input class="input" type="text" placeholder="Pesquisar um cliente">
+            <input
+              class="input"
+              type="text"
+              placeholder="Pesquisar um cliente"
+              v-model="query"
+            />
             <div class="icon is-medium is-left">
               <font-awesome-icon icon="search" />
             </div>
@@ -19,12 +24,19 @@
           </button>
         </div>
       </div>
-      <Table :headers="headers" :data="clients" @row-click="rowClick" />
+      <Table v-if="clients.length > 0" :headers="headers" :data="clients" @row-click="rowClick" />
+      <div v-else class="clients clients-empty">
+        <h3 class="subtitle is-5">Não encontramos nenhum cliente.</h3>
+        <p>
+          <img alt="empty results" title="Nao encontrado" src="../assets/images/empty.svg" />
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { debounce } from 'lodash';
 import Table from '@/components/Table.vue';
 import Form from '@/components/clients/Form.vue';
 import Menu from '@/components/Menu.vue';
@@ -35,6 +47,7 @@ export default {
   data() {
     return {
       salon: {},
+      query: '',
       allClients: [],
       clients: [],
       defaultClient: {
@@ -72,9 +85,15 @@ export default {
     Table,
     Form,
   },
+  watch: {
+    query() {
+      this.debounceSearch();
+    },
+  },
   created() {
     this.client = { ...this.defaultClient };
     this.$emit('set-loading-overlay', true);
+    this.debounceSearch = debounce(this.search, 500);
     const salon = window.localStorage.getItem('salon') || '{}';
     this.salon = JSON.parse(salon) || {};
     this.getClients();
@@ -137,6 +156,25 @@ export default {
           this.$emit('set-form-errors', this.errors);
         });
     },
+    search() {
+      this.$emit('set-loading-overlay', true);
+      if (this.query && this.query.length > 1) {
+        api.get(`/salons/${this.salon.id}/clients/search/${this.query}`)
+          .then((response) => {
+            this.clients = response.data || [];
+            this.$emit('set-loading-overlay', false);
+          })
+          .catch(() => {
+            this.$emit('set-loading-overlay', false);
+            this.$toast.open({
+              message: 'Não foi possível encontrar os clientes buscados!',
+              type: 'is-danger',
+            });
+          });
+      } else {
+        this.getClients();
+      }
+    },
     rowClick(client) {
       this.$router.push(`/clients/${client.id}`);
     },
@@ -172,6 +210,21 @@ export default {
           z-index: 10;
           font-size: 18px;
         }
+      }
+    }
+  }
+
+  .clients-empty {
+
+    h3 {
+      margin-bottom: 50px;
+    }
+
+    p {
+      height: 60vh;
+
+      img {
+        height: 100%;
       }
     }
   }
