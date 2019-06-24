c5f345be378c86ad6de0ae71a1ae5d2b4b646af9
Nelio Carneiro <neliojrr@gmail.com>
2019-06-24 09:57:23 -0300

    Checkout area and client select

---
+++ src/components/sales/CheckoutArea.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="checkout-area box">
+    <section class="columns is-multiline">
+      <section class="column is-full client-section">
+        <ClientSelect @setClient="$emit('set-client')" />
+      </section>
+      <section class="column is-full">
+        <div class="field">
+          <label class="label">Valor</label>
+          <div class="control has-icons-left">
+            <input class="input is-medium" type="text" :value="displayRawMoney(total)" />
+            <span class="icon is-small is-left">
+              <font-awesome-icon icon="dollar-sign" />
+            </span>
+          </div>
+        </div>
+      </section>
+      <section class="column is-full payment-type">
+        <div class="payment-buttons">
+          <button class="button is-primary is-medium">
+            Dinheiro
+          </button>
+          <button class="button is-primary is-medium">
+            Outro
+          </button>
+        </div>
+        <div class="payment-buttons">
+          <button class="button is-primary is-medium">
+            Cartão Débito
+          </button>
+          <button class="button is-primary is-medium">
+            Cartão Crédito
+          </button>
+        </div>
+      </section>
+    </section>
+  </div>
+</template>
+
+<script>
+import ClientSelect from '@/components/sales/ClientSelect.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    total: {
+      type: Number,
+      default: 0,
+    },
+  },
+  components: {
+    ClientSelect,
+  },
+};
+</script>
+
+<style lang="scss">
+.checkout-area {
+  height: 100%;
+
+  .client-section {
+    padding-top: 1em;
+    padding-bottom: 2em;
+    border-bottom: 1px solid #eee;
+  }
+
+  .payment-type {
+    margin-top: 2em;
+
+    .payment-buttons {
+      display: flex;
+      flex-flow: row;
+      flex-wrap: wrap;
+      justify-content: space-between;
+
+      button {
+        width: 48%;
+      }
+    }
+
+    .payment-buttons + .payment-buttons {
+      margin-top: 1em;
+    }
+  }
+
+  .cancel {
+    vertical-align: bottom;
+
+    a {
+      color: red;
+    }
+  }
+}
+</style>
---
+++ src/components/sales/ClientSelect.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="client-select">
+    <b-autocomplete
+      v-model="name"
+      :data="filteredDataObj"
+      :open-on-focus="true"
+      ref="autocomplete"
+      field="name"
+      icon="search"
+      size="is-medium"
+      placeholder="Pesquisar Cliente"
+      @select="setClient"
+    >
+      <template slot="header">
+        <a>
+          <span> Adicionar novo cliente </span>
+        </a>
+      </template>
+      <template slot="empty">Sem resultados para {{ name }}</template>
+    </b-autocomplete>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+    };
+  },
+  computed: {
+    filteredDataObj() {
+      return this.clients.filter(option => option.name
+        .toString()
+        .toLowerCase()
+        .indexOf(this.name.toLowerCase()) >= 0);
+    },
+    clients() {
+      return this.$store.state.client.all;
+    },
+  },
+  methods: {
+    setClient(client) {
+      this.$emit('set-client', client);
+    },
+  },
+};
+</script>
