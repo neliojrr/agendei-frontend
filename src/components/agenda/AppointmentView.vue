495dc54fc7fd01b8b37abcb2879271ab0133b17d
Nelio Carneiro <neliojrr@gmail.com>
2019-08-07 20:04:42 -0300

    Refactoring appointment view on mobile

--- src/components/agenda/AppointmentView.vue
+++ src/components/agenda/AppointmentView.vue
@@ -4,26 +4,34 @@
       {{ moment.unix(data.start_at).format('dddd, DD MMM YYYY') }}
     </h2>
     <div class="columns is-mobile is-vcentered">
-      <div class="column is-10-desktop is-10-tablet is-9-mobile">
+      <div class="column is-10-desktop is-10-tablet is-8-mobile">
         <div class="columns is-multiline">
-          <p class="column is-12-mobile is-2-desktop is-2-tablet appointment-time">
+          <p
+            class="column is-12-mobile is-2-desktop is-2-tablet appointment-time"
+          >
             {{ moment.unix(data.start_at).format('HH:mm') }}
           </p>
           <p class="column is-8-desktop is-8-tablet appointment-client">
             {{ data.client ? data.client.name : 'Cliente' }}
           </p>
-          <p class="column is-8-desktop is-8-tablet is-offset-2 appointment-service">
+          <p
+            class="column is-8-desktop is-8-tablet is-offset-2 appointment-service"
+          >
             {{ duration() }} com
             {{ data.employee ? data.employee.name : 'Profissional' }}
           </p>
         </div>
       </div>
-      <div class="column is-2-desktop is-2-tablet is-3-mobile">
+      <div class="column is-2-desktop is-2-tablet is-4-mobile">
         <div class="columns is-multiline">
-          <p class="column is-12-mobile is-12-desktop is-2-tablet appointment-price">
+          <p
+            class="column is-12-mobile is-12-desktop is-2-tablet appointment-price"
+          >
             {{ displayMoney(data.price) }}
           </p>
-          <p class="column is-12-desktop is-12-tablet appointment-service has-text-primary">
+          <p
+            class="column is-12-desktop is-12-tablet appointment-service has-text-primary"
+          >
             {{ data.status_display }}
           </p>
         </div>
@@ -46,20 +54,23 @@ export default {
         service: {},
         client: {},
         employee: {},
-        duration: '',
-      }),
-    },
+        duration: ''
+      })
+    }
   },
   computed: {
     moment() {
       return moment;
-    },
+    }
   },
   methods: {
     duration() {
-      return moment.duration(this.data.duration, 's').locale('pt').humanize();
-    },
-  },
+      return moment
+        .duration(this.data.duration, 's')
+        .locale('pt')
+        .humanize();
+    }
+  }
 };
 </script>
 
