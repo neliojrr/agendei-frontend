1b7e2c362faf7f92abb68658a10ed7167efa45a8
Nelio Carneiro <neliojrr@gmail.com>
2019-08-12 16:57:00 -0300

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,26 +1,66 @@
 <template>
   <footer class="footer">
-    <div class="content has-text-centered">
-      <p>
-        <span>
-          <strong>Agendei</strong>
-          por <a href="mailto:nelio@firstbits.com.br">FirstBits</a>.
-          Todos os direitos reservados.
-        </span>
-        <span class="icons">
-          <a target="_blank" href="http://twitter.com/agendei">
-            <font-awesome-icon :icon="['fab', 'twitter-square']" />
-          </a>
-          <a target="_blank" href="http://instagram.com/agendei.io">
-            <font-awesome-icon :icon="['fab', 'instagram']" />
-          </a>
-        </span>
-      </p>
+    <div class="container">
+      <div class="content columns is-mobile is-multiline">
+        <div class="agendei column is-2-desktop is-12-touch is-flex">
+          <h3 class="subtitle is-4">Agendei</h3>
+          <span class="icons">
+            <a target="_blank" href="http://twitter.com/agendei">
+              <font-awesome-icon :icon="['fab', 'twitter-square']" />
+            </a>
+            <a target="_blank" href="http://instagram.com/agendei.io">
+              <font-awesome-icon :icon="['fab', 'instagram']" />
+            </a>
+          </span>
+        </div>
+        <div class="column is-offset-1-desktop is-6-mobile company">
+          <h4 class="subtitle is-6">Empresa</h4>
+          <ul>
+            <li>
+              <a href="/">Página Inicial</a>
+            </li>
+            <li>
+              <a href="https://blog.agendei.io">Blog</a>
+            </li>
+            <li>
+              <a href="mailto:contato@agendei.io">Contato</a>
+            </li>
+            <li>
+              <a href="https://agendei.io/termos">Termos de Uso</a>
+            </li>
+            <li>
+              <a href="https://agendei.io/privacidade">
+                Política de Privacidade
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="column company is-6-mobile">
+          <h4 class="subtitle is-6">Produto</h4>
+          <ul>
+            <li>
+              <a href="https://agendei.io/cadastrar">Cadastrar</a>
+            </li>
+            <li>
+              <a href="https://agendei.io/entrar">Entrar</a>
+            </li>
+            <li>
+              <a href="https://agendei.io/precos">Preços</a>
+            </li>
+          </ul>
+        </div>
+        <div class="column copyright is-flex">
+          <span>
+            <strong>Agendei</strong>
+            por <a href="mailto:nelio@firstbits.com.br">FirstBits</a>.<br />
+            Todos os direitos reservados.
+          </span>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
 .footer {
   position: absolute;
@@ -36,22 +76,68 @@
   .content {
     color: #d9d9d9;
 
-    .icons {
-      margin-left: 20px;
+    .agendei {
+      flex-direction: column;
+      border-right: 1px solid white;
+      justify-content: center;
+      height: 50%;
+      margin: auto;
 
-      a {
-        font-size: 20px;
+      @media screen and (max-width: 768px) {
+        border: 0;
       }
 
-      a + a {
-        margin-left: 10px;
+      h3 {
+        color: #ffffff;
+      }
+
+      .icons {
+        a {
+          font-size: 20px;
+          color: white;
+          font-size: 36px;
+        }
+
+        a + a {
+          margin-left: 10px;
+        }
       }
     }
 
-    p {
+    .company {
+      text-align: left;
+
+      @media screen and (max-width: 768px) {
+        text-align: center;
+      }
+
+      h4 {
+        color: white;
+        margin-bottom: 0;
+      }
+
+      ul {
+        list-style: none;
+        margin-left: 0;
+
+        a {
+          color: white;
+          font-size: 13px;
+        }
+
+        a:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+
+    .copyright {
+      align-items: center;
+      justify-content: center;
+      font-size: 14px;
 
       strong {
-        color: #FFFFFF;
+        color: #ffffff;
       }
 
       a {
@@ -66,4 +152,3 @@
   }
 }
 </style>
-
